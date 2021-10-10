e69b1f8a8a6bedb7f385c2cd0e185cf08e7101f2
Suthan <suthankumar1194@gmail.com>
2021-10-10 11:39:58 +0530

    DB URL

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const { DB_URL } = require("./constants/config");
 
 const StudentRoutes = require("./routes/student");
 
@@ -15,7 +16,7 @@ app.listen(3000, () => {
   console.log("Express running in port 3000");
 });
 
-mongoose.connect("mongodb+srv://admin:AFjJDFwe5Ymfm7nJ@cluster0.woyfv.mongodb.net/studentdb?retryWrites=true&w=majority", (err) => {
+mongoose.connect(DB_URL, (err) => {
   if (err) {
     console.log(err);
     process.exit(0);
@@ -23,6 +24,5 @@ mongoose.connect("mongodb+srv://admin:AFjJDFwe5Ymfm7nJ@cluster0.woyfv.mongodb.ne
 
   console.log("Mongoose Connected.");
 });
-
 
 // AFjJDFwe5Ymfm7nJ
---
+++ constants/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DB_URL:
+    "mongodb+srv://admin:AFjJDFwe5Ymfm7nJ@cluster0.woyfv.mongodb.net/studentdb?retryWrites=true&w=majority",
+};
